49d4840c1a24a8093c6edce16842387b163fcd92
Prajjwal <shubhu1621999@gmail.com>
2026-01-17 18:59:19 +0530

    small changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -20,7 +20,7 @@ export const authConfig: NextAuthConfig = {
         sameSite: 'lax',
         path: '/',
         secure: true,
-        domain: 'whisper-box.xyz' 
+        // domain: 'whisper-box.xyz' 
       }
     }
   },
--- src/proxy.ts
+++ src/proxy.ts
@@ -13,6 +13,7 @@ export async function proxy(request: NextRequest) {
   const token = await getToken({ 
     req: request, 
     secret: process.env.AUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === 'production',
   });
 
   if (token && (
